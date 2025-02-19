b86dce1d8a3c64f514caea39880710fb5432bf59
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-19 16:21:01 +0100

    Fix single_list controller to fetch artist by ID and display singles by artist

--- src/Controller/SingleController.php
+++ src/Controller/SingleController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\SingleRepository;
+use App\Repository\ArtistRepository;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,16 +17,26 @@ final class SingleController extends AbstractController
 
 
     #[Route('/single/{artist_id}', name: 'single_list')]
-    public function artistList(SingleRepository $singleRepository, Request $request): Response
+    public function artistList(SingleRepository $singleRepository, ArtistRepository $artistRepository, Request $request): Response
     {
         $artist_id = $request->get('artist_id');
-        $singles = $singleRepository->findAll();
+        
+        $artist = $artistRepository->find($artist_id);
+        
+        if (!$artist) {
+            throw $this->createNotFoundException('Artist not found');
+        }
+    
+        $singles = $singleRepository->findBy(['artist' => $artist], ['releaseDate' => 'DESC']);
+    
         $vars = [
             'singles' => $singles,
+            'artist' => $artist
         ];
         
         return $this->render('single/single_list.html.twig', $vars);
     }
+    
 
     
 }
