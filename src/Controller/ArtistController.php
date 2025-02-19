8f0c789f2f2b3e2a785f3897636688f0ce7a77e9
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-19 15:47:41 +0100

    Add artistSingle controller method to fetch and display artist details

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -7,6 +7,8 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
 
 final class ArtistController extends AbstractController
 {
@@ -26,6 +28,37 @@ final class ArtistController extends AbstractController
         $artists = $artistRepository->findAll();
         $vars = [
             'artists' => $artists,
+        ];
+        
+        return $this->render('artist/artist_list.html.twig', $vars);
+    }
+
+    #[Route('/artist/{artist_id}', name: 'artist_single')]
+    public function artistSingle(ArtistRepository $artistRepository, Request $request): Response
+    {
+        $artist_id = $request->get('artist_id');
+        $artistDetails = $artistRepository->find($artist_id);
+        if (!$artistDetails) {
+            throw $this->createNotFoundException('Artist not found');
+        }
+
+        $artistData = [];
+        foreach ($artistDetails as $artistData) {
+            $toolReviewData[] = [
+                'id' => $artistData->getId(),
+                'name' => $artistData->getArtistName(),
+                'firstName' => $artistData->getFirsttName(),
+                'lastName' => $artistData->getLastName(),
+                'birthdate' => $artistData->getBirthDate(),
+                'bio' => $artistData->getBio(),
+                'album' => $artistData->getAlbums(),
+                'single' => $artistData->getSingle(),
+                'isBand'=>$artistData->getIsBand()
+            ];
+        }
+        
+        $vars = [
+            'artist' => $artistData,
         ];
         
         return $this->render('artist/artist_list.html.twig', $vars);
