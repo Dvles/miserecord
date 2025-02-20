47c9e4cf394062465b2e8aeb2e057b27628acb5f
Dvles <132080646+Dvles@users.noreply.github.com>
2025-02-20 15:26:35 +0100

    Created product list view

--- migrations/Version20250220142433.php
+++ migrations/Version20250220142433.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250220141939 extends AbstractMigration
+final class Version20250220142433 extends AbstractMigration
 {
     public function getDescription(): string
     {
